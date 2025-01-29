36f3063b39427161d154a47bba39924dcf9e0f75
Sebastian Böhme <sebastianboehme92@gmail.com>
2025-01-29 12:26:53 +0100

    Added ngClass for practice

--- src/app/fruitlist/fruitlist.component.ts
+++ src/app/fruitlist/fruitlist.component.ts
@@ -1,14 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { SinglefruitComponent } from './singlefruit/singlefruit.component';
 
 @Component({
   selector: 'app-fruitlist',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SinglefruitComponent],
   templateUrl: './fruitlist.component.html',
   styleUrl: './fruitlist.component.scss'
 })
 export class FruitlistComponent {
+
+  fontColorGood = 'green';
+  fontColorBad = 'red';
 
   fruitlist = [
     {
@@ -17,7 +21,7 @@ export class FruitlistComponent {
       description: "Äpfel sind aufgrund ihres hohen Wassergehalts kalorienarm und enthalten nur Spuren von Fett und Eiweiß, dafür aber rund zwei Prozent Ballaststoffe und etwa elf Prozent Kohlenhydrate. Äpfel enthalten auch viele Vitamine und Mineralstoffe und sind daher eine wichtige Quelle für uns - zum Beispiel für Vitamin C.",
       genus: "Kernobstgewächsen innerhalb der Familie der Rosengewächse",
       stars: 2.3,
-      reviews:[{name: "Kevin W.", text: "ist lecker"},{name: "Arne P.", text: "nicht so meins"}],
+      reviews:[],
     },
     {
       name: "Banane",
